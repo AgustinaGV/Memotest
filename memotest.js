d7319061b6e4551cf4a6608906c10594122f6531
tas014 <francoapicco@gmail.com>
2019-10-03 11:18:53 -0300

    Update memotest.js

--- memotest.js
+++ memotest.js
@@ -1,2 +1,19 @@
 // DECLARO VARIABLES Y CONSTANTES;
 
+var matrixSize=5; //Dimension de la matriz (5x5, 6x6, etc)
+var matrix=[]; // declaro la matriz
+
+for (i=0; i<matrixSize, i++) { //recorre la dimension de la matriz
+    matrix.push([]);//agrega un array por posicion de matrixSize
+    for (x=0;x<i;x++) {
+        matrix[i].push(0);//rellena de la cantidad de 0 necesaria
+    }
+}
+
+for (i=0; i=board.length;i++) {
+    valueAssigner = dameRandom(maxValue);
+    imgPath = i;
+    for (x=0;x<2;x++) {
+        board[dameRandom(boardCap)][dameRandom(boardCap)] = valueAssigner;        
+    }
+}
