88e3b468d4a722ef0d33ee3bc6d9537ea05d6509
AgustinaGV <agustinagarciavega@gmail.com>
2019-10-10 11:06:05 -0300

    update

--- memotest.js
+++ memotest.js
@@ -1,5 +1,7 @@
 // DECLARO VARIABLES Y CONSTANTES;
 var matrixSize=5; //Dimension de la matriz (5x5, 6x6, etc)
+var matrixRows = 5; //Dimension de la matriz (5x5, 6x6, etc)
+var matrixColumns = matrixRows;
 var matrix=[]; // declaro la matriz
 var puntos; //
 var tiempo; //declaro variable que guarda el tiempo
@@ -9,34 +11,31 @@ var jugadoresTiempo=[]; //declaro un array donde llevo el tiempo de cada jugador
 var jugadoresPuntaje=[]; //declaro un array donde llevo el puntaje de cada jugador.
 var baseScore=1000; // puntaje base
 const modifier=0.95; // El valor por el cual se divide el puntaje por cada segundo que el jugador tardo.
-for (i=0; i<matrixSize, i++) { //recorre la dimension de la matriz
-   matrix.push([]);//agrega un array por posicion de matrixSize
-   for (x=0;x<i;x++) {
-       matrix[i].push(0);//rellena de la cantidad de 0 necesaria
-   }
-};
-for (i=0; i=board.length;i++) {
-   valueAssigner = dameRandom(maxValue);
-   imgPath = i;
-   for (x=0;x<2;x++) {
-       board[dameRandom(boardCap)][dameRandom(boardCap)] = valueAssigner;
-   }
-};
+
+function generateMatrix () {
+    for (let i=0; i<matrixRows; i++) { //recorre la dimension de la matriz
+    matrix.push([]);//agrega un array por posicion de matrixSize
+    for (let j=0; j<matrixColumns; j++) {
+        matrix[i].push(0);//rellena de la cantidad de 0 necesaria
+        }
+    }
+}
+
+function asignateImgs () {
+    for (i=0; i=board.length;i++) {
+    valueAssigner = dameRandom(maxValue);
+    imgPath = i;
+    for (x=0;x<2;x++) {
+        board[dameRandom(boardCap)][dameRandom(boardCap)] = valueAssigner;
+        }
+    }
+}
 //Lógica de jugador - Time Deathmatch
 function contraReloj (tiempo) {
        generarJugadores();
        clock=setInterval(raceTime(tiempo),1000); // Comienza la cuenta del reloj
 };
-function raceTime(tiempo,id) { //toda esta funcion es para que en vez de comparar puntajes al terminar, que el jugador pueda ver un reloj contando hacia atrás para saber cuanto tiempo lo queda para vencer a los otros jugadores.
-   if (tiempo==0) {
-       finish();
-       document.getElementById(id).innerHTML="0";
-   }
-   else{
-   tiempo= tiempo-1;
-   document.getElementById(id).innerHTML=JSON.stringify(tiempo); //imprime el tiempo en el elemento HTML que necesitemos (cada segundo que pasa)
-   }
-};
+
 function finish() {
    clearInterval(clock); // freno el reloj
    displayScore(/* insertar id del elemento que muestra el puntaje */); // dependiendo de cómo querramos avanzar la parte visual esta funcion va a hacerse un poco mas larga
